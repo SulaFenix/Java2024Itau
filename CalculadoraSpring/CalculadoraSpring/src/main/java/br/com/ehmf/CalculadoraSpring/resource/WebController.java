3d050eeaa3ff691205feb865094f0b721b124574
Suellen Gularte <sula.fenix@gmail.com>
2024-03-11 22:07:30 -0300

    Aula 20_02

--- CalculadoraSpring/CalculadoraSpring/src/main/java/br/com/ehmf/CalculadoraSpring/resource/WebController.java
+++ CalculadoraSpring/CalculadoraSpring/src/main/java/br/com/ehmf/CalculadoraSpring/resource/WebController.java
@@ -1,15 +1,50 @@
 package br.com.ehmf.CalculadoraSpring.resource;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
 	
 	@GetMapping("/")
 	public String showForm(Model model) {
-		return "form";
+		model.addAttribute("modelOperacoes", List.of("Soma", "Subtracao"));
+		return "index";
+	}
+	
+	@PostMapping("/")
+	public String handleFormSubmission(@RequestParam String modelOperacoes, 
+																@RequestParam String valor01,
+																@RequestParam String valor02,
+																Model model) {
+		
+		if(modelOperacoes.isEmpty() || valor01.isEmpty() || valor02.isEmpty()) {
+			model.addAttribute("response","Existem campos em branco!");
+			return "index";
+		}
+		
+		String resposta = "";
+		int res = 0;
+		
+		switch (modelOperacoes) {
+		case "Soma": 
+				res = Integer.parseInt(valor01) + Integer.parseInt(valor02);
+				break;
+		case "Subtracao": 
+				res = Integer.parseInt(valor01) + Integer.parseInt(valor02);
+				break;
+		}
+				
+		resposta = String.valueOf(res);
+		model.addAttribute("modelOperacoes", List.of("Soma", "Subtracao"));
+		model.addAttribute("response", resposta);
+		return "index";
+		
 	}
 
 }
